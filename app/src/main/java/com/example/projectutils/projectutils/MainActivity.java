77ef54b92a2f322b5745ef91aa60d07ce63600f3
apple <18201497620@163.com>
2021-03-26 00:57:06 +0800

    修改Jcenter上传配置改为阿里云maven

--- app/src/main/java/com/example/projectutils/projectutils/MainActivity.java
+++ app/src/main/java/com/example/projectutils/projectutils/MainActivity.java
@@ -1,9 +1,10 @@
-package com.example.projectlib;
+package com.example.projectutils.projectutils;
 
-import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.example.projectutils.R;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
